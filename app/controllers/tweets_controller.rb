67f9f16fc0e14ef7e2714544468e502b25960660
wakana-lab <2312032e@gsuite.kobe-u.ac.jp>
2025-05-20 09:31:13 +0900

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,54 @@
+class TweetsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @tweets = Tweet.all
+    search = params[:search]
+    if search.present?
+      @tweets = @tweets.joins(:user).where("body LIKE ? OR name LIKE ?", "%#{search}%", "%#{search}%")
+    end
+    @tweets = @tweets.page(params[:page]).per(3)
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    tweet = Tweet.new(tweet_params)
+    tweet.user_id = current_user.id
+    if tweet.save
+      redirect_to :action => "index"
+    else
+      redirect_to :action => "new"
+    end
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.update(tweet_params)
+      redirect_to :action => "show", :id => tweet.id
+    else
+      redirect_to :action => "new"
+    end
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to action: :index
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:body,:image)
+  end
+end
---
+++ app/models/tweet.rb
@@ -0,0 +1,4 @@
+class Tweet < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+end
